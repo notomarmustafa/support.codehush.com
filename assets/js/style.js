322da4e417939644917d731fcb81cb3716a06f78
Omar <omarmustafa2024@gmail.com>
2024-09-11 14:12:21 -0400

    init

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,9 +5,9 @@ const searchInput = document.querySelector("[data-search]")
 let users = []
 
 searchInput.addEventListener("input", e => {
-    const value = e.target.value
+    const value = e.target.value.toLowerCase()
     users.forEach(user => {
-        const isVisible = user.name.includes(value) || user.email.includes(value)
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
         user.element.classList.toggle("hide", !isVisible)
     })
 })
